b308c3c783f9d1fcf27187e486a2c3d4a02e0665
feixia5320 <1044489176@qq.com>
2020-04-14 23:11:34 +0800

    表单组件

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,10 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+// eslint-disable-next-line
 import Game from "./component/game/game";
+import MySelect from './component/form/select'
+import Reservation from './component/form/form2'
 
+let arr = [
+  <Game />,
+  <MySelect />,
+  <Reservation />,
+]
 
 ReactDOM.render(
-  <Game />,
+  arr,
   document.getElementById('root')
 );
