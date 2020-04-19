4d2d2fc3620f09ba77cc5925dae1bf0d031a2edb
feixia5320 <1044489176@qq.com>
2020-04-19 17:40:03 +0800

    context

---
+++ my-app/src/component/context-multi/app.js
@@ -0,0 +1,58 @@
+import React from 'react'
+// Theme context，默认的 theme 是 “light” 值
+const ThemeContext = React.createContext('light');
+
+// 用户登录 context
+const UserContext = React.createContext({
+  name: 'Guest',
+});
+
+class ContextApp extends React.Component {
+  render() {
+    const {signedInUser, theme} = this.props;
+
+    // 提供初始 context 值的 App 组件
+    return (
+      <ThemeContext.Provider value={theme}>
+        <UserContext.Provider value={signedInUser}>
+          <Layout />
+        </UserContext.Provider>
+      </ThemeContext.Provider>
+    );
+  }
+}
+
+function Layout() {
+  return (
+    <div>
+      <Content />
+    </div>
+  );
+}
+
+// 一个组件可能会消费多个 context
+function Content() {
+  return (
+    <ThemeContext.Consumer>
+      {theme => (
+        <UserContext.Consumer>
+          {user => (
+            <ProfilePage user={user} theme={theme} />
+          )}
+        </UserContext.Consumer>
+      )}
+    </ThemeContext.Consumer>
+  );
+}
+
+function ProfilePage(props) {
+    return (
+        <p>{props.user}{props.theme}</p>
+    )
+}
+function test () {
+    return(
+        <ContextApp signedInUser="sdfsdf" theme="theme"/>
+    )
+}
+export default test;
--- my-app/src/router.js
+++ my-app/src/router.js
@@ -3,12 +3,21 @@ import {HashRouter, Route, Switch} from 'react-router-dom';
 
 import MySelect from './component/form/2-select'
 import Reservation from './component/form/1-form2'
+import ContextApp1 from './component/form/6-context'
+import ContextApp2 from './component/context/app'
+import ContextApp3 from './component/context2/app'
+import ContextApp4 from './component/context-multi/app'
 
 const MyRouter = () => (
     <HashRouter>
         <Switch>
             <Route exact path="/c" component={MySelect}/>
             <Route exact path="/d" component={Reservation}/>
+            <Route exact path="/cont1" component={ContextApp1}/>
+            <Route exact path="/cont2" component={ContextApp2}/>
+            <Route exact path="/cont3" component={ContextApp3}/>
+            <Route exact path="/cont4" component={ContextApp4}/>
+            
         </Switch>
     </HashRouter>
 );
