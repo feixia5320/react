7a90fe9b51b379fc8f018da9b559e57888c2d7be
feixia5320 <1044489176@qq.com>
2020-12-10 23:34:35 +0800

    tt

--- my-app/src/component/ref/1-ref.js
+++ my-app/src/component/ref/1-ref.js
@@ -4,16 +4,23 @@ import React from 'react'
 class Ref1 extends React.Component {
     constructor(props) {
         super(props);
+        this.myInputRef22 = React.createRef();
         this.handleClick = this.handleClick.bind(this)
     }
     // 使用原生dom获取焦点
     handleClick() {
+        // 写法1
         this.refs.myInputRef.focus();
+        setTimeout(() => {
+            // 写法2
+            this.myInputRef22.current.focus();
+        }, 2000);
     }
     render() {
         return (
             <div>
                 <input type="text" ref="myInputRef" />
+                <input type="text" ref={this.myInputRef22} />
                 <input type="button" value="获取焦点"
                     onClick={this.handleClick} />
             </div>
@@ -54,35 +61,9 @@ class Ref2 extends React.Component {
         )
     }
 }
-
-function Component() {
-    return (
-        <input type="text" value="aa" />
-    )
-}
-
-function logProps(Component) {
-    class LogProps extends React.Component {
-        componentDidUpdate(prevProps) {
-            console.log('old props:', prevProps);
-            console.log('new props:', this.props);
-        }
-
-        render() {
-            const { forwardedRef, ...rest } = this.props;
-            // 将自定义的 prop 属性 “forwardedRef” 定义为 ref
-            // TODO forwardedRef
-            return <Component ref={forwardedRef} {...rest} />;
-        }
-    }
-    // 注意 React.forwardRef 回调的第二个参数 “ref”。
-    // 我们可以将其作为常规 prop 属性传递给 LogProps，例如 “forwardedRef”
-    // 然后它就可以被挂载到被 LogProps 包裹的子组件上。
-    return React.forwardRef((props, ref) => {
-        return <LogProps {...props} forwardedRef={ref} />;
-    });
-}
-
+/**
+ * ref3转发
+ */
 class Ref3 extends React.Component {
     constructor(props) {
         super(props);
@@ -92,7 +73,7 @@ class Ref3 extends React.Component {
     // 使用原生dom获取焦点
     handleClick() {
         console.log(this.testref)
-        // this.testref.current.focus();
+        this.testref.current.focus();
     }
 
     render() {
@@ -107,6 +88,34 @@ class Ref3 extends React.Component {
             </div>
         )
     }
+}
+
+function Component(props) {
+    return (
+        <input type="text" value="" ref={props.forwardedRef} />
+    )
+}
+
+// ref与key类似，不会像props往下传递，所以需要吧ref转换为用props方式传递
+function logProps(Component) {
+    class LogProps extends React.Component {
+        componentDidUpdate(prevProps) {
+            console.log('old props:', prevProps);
+            console.log('new props:', this.props);
+        }
+
+        render() {
+            const props = this.props;
+            // 将自定义的 prop 属性 “forwardedRef” 定义为 ref
+            return <Component {...props} />;
+        }
+    }
+    // 注意 React.forwardRef 回调的第二个参数 “ref”。
+    // 我们可以将其作为常规 prop 属性传递给 LogProps，例如 “forwardedRef”
+    // 然后它就可以被挂载到被 LogProps 包裹的子组件上。
+    return React.forwardRef((props, ref) => {
+        return <LogProps {...props} forwardedRef={ref} />;
+    });
 }
 
 function out() {
